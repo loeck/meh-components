fe2ef0ed048069b6c1a83381fdbb51e675a81ec0
Loëck Vézien <loeck.vezien@ekino.com>
2017-12-10 21:09:11 +0100

    Fix Box

--- src/Box/index.js
+++ src/Box/index.js
@@ -4,8 +4,8 @@ import styled from 'styled-components'
 const Box = styled.div`
   align-items: ${p => p.align};
   display: flex;
-  flex-direction: ${p => !p.horizontal && 'column'};
-  flex-grow: ${p => p.grow && 1};
+  flex-direction: ${p => (!p.horizontal ? 'column' : null)};
+  flex-grow: ${p => (p.grow ? 1 : null)};
   justify-content: ${p => p.justify};
   ${p =>
     p.sticky &&
@@ -14,13 +14,13 @@ const Box = styled.div`
     left: 0;
     right: 0;
     top: 0;
-  `} overflow: ${p => p.scrollable && 'auto'};
-  padding: ${p => p.padding && `${p.padding}px`};
-  position: ${p => (p.relative ? 'relative' : p.sticky && 'absolute')};
+  `} overflow: ${p => (p.scrollable ? 'auto' : null)};
+  padding: ${p => (p.padding ? `${p.padding}px` : null)};
+  position: ${p => (p.relative ? 'relative' : p.sticky ? 'absolute' : null)};
 
   > * + * {
-    margin-left: ${p => p.horizontal && p.flow && `${p.flow}px`};
-    margin-top: ${p => !p.horizontal && p.flow && `${p.flow}px`};
+    margin-left: ${p => (p.horizontal && p.flow ? `${p.flow}px` : null)};
+    margin-top: ${p => (!p.horizontal && p.flow ? `${p.flow}px` : null)};
   }
 `
 
